f453c8c352724b71fca1101e20d477e7ad0eb914
nkwaaaa <nkwaaaa@gmail.com>
2023-01-30 18:35:46 -0300

    add react-toastify

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Button() {
     const [value, setValue] = useState(true);
 
+    const handleClick = () => {
+        setValue(!value);
+
+        toast("Wow so easy!");
+    };
+
     return (
-        <button
-            onClick={() => setValue(!value)}
-            type="button"
-            class="   py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-        >
-            Ahora {value ? "sí" : "no"}
-        </button>
+        <>
+            <button
+                onClick={handleClick}
+                type="button"
+                class="   py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+            >
+                Ahora {value ? "sí" : "no"}
+            </button>
+            <ToastContainer />
+        </>
     );
 }
